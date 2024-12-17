d6fc38324f38d618622fb5ec99f8164f95746822
Giorgi Ilariani <codemasterig@gmail.com>
2024-12-17 15:53:48 +0400

    working on verify email modal

--- components/shared/layout/header.tsx
+++ components/shared/layout/header.tsx
@@ -8,13 +8,20 @@ import React, { useState } from "react";
 import Modal from "@/components/shared/RegistrationModal";
 import { LogInForm } from "@/components/forms/LogInForm";
 import { SignUpWithEmailForm } from "@/components/forms/SignUpWithEmailForm";
+import { Input } from "@/components/ui/input";
+import { usePathname } from "next/navigation";
 
 const HeaderComponent = () => {
+  const pathname = usePathname();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isSignUpWithEmail, setIsSignUpWithEmail] = useState(false);
   const [authMethod, setAuthMethod] = useState<"sign-up" | "sign-in" | null>(
     null
   );
+  const [code, setCode] = useState("");
+
+  // Simulate authenticated status for showcasing
+  const isAuthenticated = true; // Change to `true` to simulate email verification view
 
   const openModal = (method: "sign-up" | "sign-in") => {
     setIsModalOpen(true);
@@ -35,7 +42,7 @@ const HeaderComponent = () => {
   return (
     <header
       className={`absolute ${
-        window.location.pathname !== "/" ? "bg-white" : "bg-[#FFFFFF33]"
+        pathname !== "/" ? "bg-white" : "bg-[#FFFFFF33]"
       } top-[18px] left-0 right-0 mx-auto max-w-[1264px] h-[73px] rounded-[21px] flex justify-between items-center px-[22px] z-10`}
     >
       <Link href="/">
@@ -49,7 +56,7 @@ const HeaderComponent = () => {
 
       <div className="flex items-center gap-7">
         {headerConstants.map((item) => {
-          const isActive = item.route === window.location.pathname;
+          const isActive = item.route === pathname;
 
           return (
             <Link
@@ -77,7 +84,35 @@ const HeaderComponent = () => {
           <div className="flex-1">
             <div className="flex flex-col py-[44px] px-[60px]">
               <div className="flex flex-col gap-[9px]">
-                {authMethod === "sign-up" ? (
+                {isAuthenticated ? (
+                  <>
+                    <h2 className="text-[28px] font-bold text-[#09121F]">
+                      Verify Your Email
+                    </h2>
+                    <p className="text-base text-[#09121F]">
+                      We've sent you Code, to sure that email address is yours.
+                      So, check inbox and continue there.{" "}
+                    </p>
+
+                    <div className="mt-[51px]">
+                      <label htmlFor="code">Enter Code</label>
+                      <Input
+                        name="code"
+                        id="code"
+                        className="bg-white h-[45px] text-[#09121F] px-6 py-[18px] text-base border-white focus:border focus:border-[#F7A33C] rounded-[10px] mt-[6px] mb-[17px]"
+                        value={code}
+                        onChange={(e) => setCode(e.target.value)}
+                      />
+                      <span className="text-[#00000040] font-medium text-base underline cursor-pointer">
+                        Resend
+                      </span>
+
+                      <Button className="w-full rounded-[10px] font-semibold text-white text-base bg-[#09121F] hover:bg-[#09121F] cursor-pointer py-[10px] px-[18px] mt-[132px]">
+                        Verify
+                      </Button>
+                    </div>
+                  </>
+                ) : authMethod === "sign-up" ? (
                   <>
                     <h2 className="text-[28px] font-bold text-[#09121F]">
                       Sign up
@@ -109,7 +144,7 @@ const HeaderComponent = () => {
                   </>
                 )}
               </div>
-              {!isSignUpWithEmail ? (
+              {!isSignUpWithEmail && !isAuthenticated ? (
                 <>
                   <div className="flex flex-col">
                     <div className="flex flex-col gap-4 mt-16">
@@ -150,9 +185,9 @@ const HeaderComponent = () => {
                     </span>
                   </p>
                 </>
-              ) : (
+              ) : isSignUpWithEmail ? (
                 <SignUpWithEmailForm />
-              )}
+              ) : null}
             </div>
           </div>
           <Image
