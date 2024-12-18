afc9d7e6096802937b792420d2042f60240ca9ad
Giorgi Ilariani <codemasterig@gmail.com>
2024-12-18 21:11:51 +0400

    adds responsive on faqs page

--- app/(root)/faqs/page.tsx
+++ app/(root)/faqs/page.tsx
@@ -47,7 +47,7 @@ const FAQsPage = () => {
   return (
     <>
       <section className="w-full bg-[#E9EBF0] pb-[125px] rounded-b-[63px]">
-        <h1 className="text-[#09121F] font-bold text-[57px] pt-[156px] text-center">
+        <h1 className="text-[#09121F] font-bold text-[40px] md:text-[57px] pt-[100px] md:pt-[156px] text-center">
           FAQs
         </h1>
 
@@ -87,36 +87,38 @@ const FAQsPage = () => {
           </div>
         )}
 
-        <div className="max-w-[1070px] mx-auto mt-[60px] flex flex-col gap-[22px]">
+        <div className="max-w-[1070px] mx-auto mt-[60px] flex flex-col gap-[22px] px-4">
           {faqsConstants.map((faq, index) => (
             <div
               key={index}
-              className="w-full bg-[#FFFFFF99] rounded-[21px] p-[22px]"
+              className="w-full bg-[#FFFFFF99] rounded-[21px] p-[15px] md:p-[22px]"
             >
               <div
-                className="bg-white flex items-center justify-between gap-4 p-[25px] rounded-[19px] cursor-pointer"
+                className="bg-white flex items-center gap-3 p-[25px] rounded-[19px] cursor-pointer flex-col"
                 onClick={() => toggleFAQ(index)}
               >
-                <h3 className="text-[25px] text-[#09121F] font-bold flex-1">
-                  {highlightText(faq.question, submittedTerm)}
-                </h3>
-                {openIndex === index ? (
-                  <IoIosArrowUp size={30} color="#09121F" />
-                ) : (
-                  <IoIosArrowDown size={30} color="#09121F" />
+                <div className="w-full flex gap-4 items-center justify-between">
+                  <h3 className="text-[20px] md:text-[25px] text-[#09121F] font-bold flex-1">
+                    {highlightText(faq.question, submittedTerm)}
+                  </h3>
+                  {openIndex === index ? (
+                    <IoIosArrowUp size={30} color="#09121F" />
+                  ) : (
+                    <IoIosArrowDown size={30} color="#09121F" />
+                  )}
+                </div>
+
+                {openIndex === index && (
+                  <p className="mt-4 text-[#09121F] text-base">{faq.answer}</p>
                 )}
               </div>
-
-              {openIndex === index && (
-                <p className="mt-4 text-[#09121F] text-base">{faq.answer}</p>
-              )}
             </div>
           ))}
         </div>
       </section>
-      <section className="w-full pt-20">
+      <section className="w-full pt-20 hidden">
         <div className="flex flex-col gap-9 max-w-[1264px] mx-auto w-full justify-center items-center">
-          <h2 className="text-[45px] font-bold text-white">
+          <h2 className="text-[35px] md:text-[45px] font-bold text-white text-center">
             Have more questions?{" "}
           </h2>
           <Button className="bg-white hover:bg-white text-[#000000] py-[10px] px-[18px] text-base font-semibold rounded-[10px]">
@@ -124,7 +126,7 @@ const FAQsPage = () => {
           </Button>
         </div>
       </section>
-      <Separator className="border border-[#F7A33C] w-full my-20 max-w-[1264px] mx-auto" />
+      <Separator className="border border-[#F7A33C] w-full my-20 max-w-[1264px] mx-auto hidden" />
     </>
   );
 };
