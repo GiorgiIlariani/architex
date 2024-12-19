66eb66af30cb9faca9165dd8e46be02ce29c5a6a
Giorgi Ilariani <codemasterig@gmail.com>
2024-12-19 18:43:47 +0400

    working on profile home page

--- app/(root)/profile/home/[id]/page.tsx
+++ app/(root)/profile/home/[id]/page.tsx
@@ -1,6 +1,8 @@
 import ProfileResultCard from "@/components/shared/ProfileResultCard";
 import { profileHomeResultConstants } from "@/constants";
+import Link from "next/link";
 import React from "react";
+import { IoArrowBackOutline } from "react-icons/io5";
 
 const HomeResultPage = async ({
   params,
@@ -14,11 +16,21 @@ const HomeResultPage = async ({
   );
 
   return (
-    <div className="mt-8">
-      <h1 className="hidden md:block text-[#09121F] text-[45px] font-bold pt-[41px]">
+    <div className="">
+      <h1 className="hidden md:block text-[#09121F] text-[45px] font-bold">
         Home
       </h1>
-      <ProfileResultCard {...card[0]} />
+      <div className="flex flex-col items-center md:flex-row gap-5 md:items-start md:gap-8 lg:gap-[49px] mt-8">
+        <Link
+          href="/profile/home"
+          className="w-full pt-7 flex items-center gap-[10px] h-8 justify-start"
+        >
+          <IoArrowBackOutline size={28} />
+          <span className="text-[#09121F] font-medium text-base">Back</span>
+        </Link>
+
+        <ProfileResultCard {...card[0]} />
+      </div>
     </div>
   );
 };
