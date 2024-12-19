168edc7ee36be96605ee0eecc53230016ee125a4
Giorgi Ilariani <codemasterig@gmail.com>
2024-12-19 18:01:13 +0400

    trying to fix type error

--- app/(root)/profile/home/[id]/page.tsx
+++ app/(root)/profile/home/[id]/page.tsx
@@ -1,22 +1,15 @@
 import ProfileResultCard from "@/components/shared/ProfileResultCard";
 import { profileHomeResultConstants } from "@/constants";
 import React from "react";
-import { NextPage } from "next";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-const page: NextPage<PageProps> = ({ params }) => {
+const HomeResultPage = ({ params }: { params: { id: string } }) => {
   const card = profileHomeResultConstants.filter(
     (item) => item.id !== Number(params.id)
   );
 
   return (
     <div className="mt-8">
-      <h1 className="hidden md:block text-[#09121F] text-[45px] font-bold">
+      <h1 className="hidden md:block text-[#09121F] text-[45px] font-bold pt-[41px]">
         Home
       </h1>
       <ProfileResultCard {...card[0]} />
@@ -24,4 +17,4 @@ const page: NextPage<PageProps> = ({ params }) => {
   );
 };
 
-export default page;
+export default HomeResultPage;
